c61bc0c166c6083d72b1f930a020b1c24e340067
mario <laserna.mario@gmail.com>
2013-10-14 17:00:21 -0500

    files y script sql de la bd

---
+++ sites/default/files/js/js_W8pnldfHZFG3W1B6strIBnUaSvBFIZqDiyozcbMcdes.js
@@ -0,0 +1,178 @@
+(function ($) {
+
+Drupal.behaviors.textarea = {
+  attach: function (context, settings) {
+    $('.form-textarea-wrapper.resizable', context).once('textarea', function () {
+      var staticOffset = null;
+      var textarea = $(this).addClass('resizable-textarea').find('textarea');
+      var grippie = $('<div class="grippie"></div>').mousedown(startDrag);
+
+      grippie.insertAfter(textarea);
+
+      function startDrag(e) {
+        staticOffset = textarea.height() - e.pageY;
+        textarea.css('opacity', 0.25);
+        $(document).mousemove(performDrag).mouseup(endDrag);
+        return false;
+      }
+
+      function performDrag(e) {
+        textarea.height(Math.max(32, staticOffset + e.pageY) + 'px');
+        return false;
+      }
+
+      function endDrag(e) {
+        $(document).unbind('mousemove', performDrag).unbind('mouseup', endDrag);
+        textarea.css('opacity', 1);
+      }
+    });
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+
+/**
+ * Toggle the visibility of a fieldset using smooth animations.
+ */
+Drupal.toggleFieldset = function (fieldset) {
+  var $fieldset = $(fieldset);
+  if ($fieldset.is('.collapsed')) {
+    var $content = $('> .fieldset-wrapper', fieldset).hide();
+    $fieldset
+      .removeClass('collapsed')
+      .trigger({ type: 'collapsed', value: false })
+      .find('> legend span.fieldset-legend-prefix').html(Drupal.t('Hide'));
+    $content.slideDown({
+      duration: 'fast',
+      easing: 'linear',
+      complete: function () {
+        Drupal.collapseScrollIntoView(fieldset);
+        fieldset.animating = false;
+      },
+      step: function () {
+        // Scroll the fieldset into view.
+        Drupal.collapseScrollIntoView(fieldset);
+      }
+    });
+  }
+  else {
+    $fieldset.trigger({ type: 'collapsed', value: true });
+    $('> .fieldset-wrapper', fieldset).slideUp('fast', function () {
+      $fieldset
+        .addClass('collapsed')
+        .find('> legend span.fieldset-legend-prefix').html(Drupal.t('Show'));
+      fieldset.animating = false;
+    });
+  }
+};
+
+/**
+ * Scroll a given fieldset into view as much as possible.
+ */
+Drupal.collapseScrollIntoView = function (node) {
+  var h = document.documentElement.clientHeight || document.body.clientHeight || 0;
+  var offset = document.documentElement.scrollTop || document.body.scrollTop || 0;
+  var posY = $(node).offset().top;
+  var fudge = 55;
+  if (posY + node.offsetHeight + fudge > h + offset) {
+    if (node.offsetHeight > h) {
+      window.scrollTo(0, posY);
+    }
+    else {
+      window.scrollTo(0, posY + node.offsetHeight - h + fudge);
+    }
+  }
+};
+
+Drupal.behaviors.collapse = {
+  attach: function (context, settings) {
+    $('fieldset.collapsible', context).once('collapse', function () {
+      var $fieldset = $(this);
+      // Expand fieldset if there are errors inside, or if it contains an
+      // element that is targeted by the URI fragment identifier.
+      var anchor = location.hash && location.hash != '#' ? ', ' + location.hash : '';
+      if ($fieldset.find('.error' + anchor).length) {
+        $fieldset.removeClass('collapsed');
+      }
+
+      var summary = $('<span class="summary"></span>');
+      $fieldset.
+        bind('summaryUpdated', function () {
+          var text = $.trim($fieldset.drupalGetSummary());
+          summary.html(text ? ' (' + text + ')' : '');
+        })
+        .trigger('summaryUpdated');
+
+      // Turn the legend into a clickable link, but retain span.fieldset-legend
+      // for CSS positioning.
+      var $legend = $('> legend .fieldset-legend', this);
+
+      $('<span class="fieldset-legend-prefix element-invisible"></span>')
+        .append($fieldset.hasClass('collapsed') ? Drupal.t('Show') : Drupal.t('Hide'))
+        .prependTo($legend)
+        .after(' ');
+
+      // .wrapInner() does not retain bound events.
+      var $link = $('<a class="fieldset-title" href="#"></a>')
+        .prepend($legend.contents())
+        .appendTo($legend)
+        .click(function () {
+          var fieldset = $fieldset.get(0);
+          // Don't animate multiple times.
+          if (!fieldset.animating) {
+            fieldset.animating = true;
+            Drupal.toggleFieldset(fieldset);
+          }
+          return false;
+        });
+
+      $legend.append(summary);
+    });
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+  
+  Drupal.behaviors.addtoany = {
+    attach: function(context, settings) {
+    
+      
+      // Initial page setup
+      
+      var addtoany_icon = $('input[value="a2a_svg_32"]').next('label').find('img:first'),
+        initial_icon_size_int = parseInt( $('input[name="addtoany_buttons_size"]:checked').val() );
+      
+      // Set the A2A icon's size to match selected Icon Size
+      addtoany_icon.height(initial_icon_size_int).width(initial_icon_size_int);
+      
+      
+      // Bring attention to large A2A icon option
+      // because the universal button will likely be changed to match the other icons
+      // (Drupal #states can't handle this)
+      $('input[name="addtoany_buttons_size"]').change(function() {
+      
+      	var icon_size = $(this).val(),
+      	  icon_size_int = parseInt(icon_size);
+        
+        // Expand Universal Button fieldset if collapsed
+        if ( $('#edit-universal-button').is('.collapsed') )
+          Drupal.toggleFieldset('#edit-universal-button');
+        
+        // Flash the A2A icon if not selected, because it should be selected to match the other icons
+        if ( icon_size == '32' && ! $('input[value="a2a_svg_32"]').is(':checked') )
+          addtoany_icon.fadeTo('fast', .2).fadeTo('fast', 1).fadeTo('fast', .2).fadeTo('fast', 1);
+        
+        // Set the A2A icon's size to match selected Icon Size
+        addtoany_icon.height(icon_size_int).width(icon_size_int);
+        
+      });
+      
+      
+    }
+  };
+
+}(jQuery));;
---
+++ sites/default/files/js/js_YrsVdxWQ-eFRz9IPuEuwzrtMYR5B_qjVfwnOTXM4qto.js
@@ -0,0 +1,290 @@
+(function ($) {
+
+Drupal.behaviors.textarea = {
+  attach: function (context, settings) {
+    $('.form-textarea-wrapper.resizable', context).once('textarea', function () {
+      var staticOffset = null;
+      var textarea = $(this).addClass('resizable-textarea').find('textarea');
+      var grippie = $('<div class="grippie"></div>').mousedown(startDrag);
+
+      grippie.insertAfter(textarea);
+
+      function startDrag(e) {
+        staticOffset = textarea.height() - e.pageY;
+        textarea.css('opacity', 0.25);
+        $(document).mousemove(performDrag).mouseup(endDrag);
+        return false;
+      }
+
+      function performDrag(e) {
+        textarea.height(Math.max(32, staticOffset + e.pageY) + 'px');
+        return false;
+      }
+
+      function endDrag(e) {
+        $(document).unbind('mousemove', performDrag).unbind('mouseup', endDrag);
+        textarea.css('opacity', 1);
+      }
+    });
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+
+/**
+ * Toggle the visibility of a fieldset using smooth animations.
+ */
+Drupal.toggleFieldset = function (fieldset) {
+  var $fieldset = $(fieldset);
+  if ($fieldset.is('.collapsed')) {
+    var $content = $('> .fieldset-wrapper', fieldset).hide();
+    $fieldset
+      .removeClass('collapsed')
+      .trigger({ type: 'collapsed', value: false })
+      .find('> legend span.fieldset-legend-prefix').html(Drupal.t('Hide'));
+    $content.slideDown({
+      duration: 'fast',
+      easing: 'linear',
+      complete: function () {
+        Drupal.collapseScrollIntoView(fieldset);
+        fieldset.animating = false;
+      },
+      step: function () {
+        // Scroll the fieldset into view.
+        Drupal.collapseScrollIntoView(fieldset);
+      }
+    });
+  }
+  else {
+    $fieldset.trigger({ type: 'collapsed', value: true });
+    $('> .fieldset-wrapper', fieldset).slideUp('fast', function () {
+      $fieldset
+        .addClass('collapsed')
+        .find('> legend span.fieldset-legend-prefix').html(Drupal.t('Show'));
+      fieldset.animating = false;
+    });
+  }
+};
+
+/**
+ * Scroll a given fieldset into view as much as possible.
+ */
+Drupal.collapseScrollIntoView = function (node) {
+  var h = document.documentElement.clientHeight || document.body.clientHeight || 0;
+  var offset = document.documentElement.scrollTop || document.body.scrollTop || 0;
+  var posY = $(node).offset().top;
+  var fudge = 55;
+  if (posY + node.offsetHeight + fudge > h + offset) {
+    if (node.offsetHeight > h) {
+      window.scrollTo(0, posY);
+    }
+    else {
+      window.scrollTo(0, posY + node.offsetHeight - h + fudge);
+    }
+  }
+};
+
+Drupal.behaviors.collapse = {
+  attach: function (context, settings) {
+    $('fieldset.collapsible', context).once('collapse', function () {
+      var $fieldset = $(this);
+      // Expand fieldset if there are errors inside, or if it contains an
+      // element that is targeted by the URI fragment identifier.
+      var anchor = location.hash && location.hash != '#' ? ', ' + location.hash : '';
+      if ($fieldset.find('.error' + anchor).length) {
+        $fieldset.removeClass('collapsed');
+      }
+
+      var summary = $('<span class="summary"></span>');
+      $fieldset.
+        bind('summaryUpdated', function () {
+          var text = $.trim($fieldset.drupalGetSummary());
+          summary.html(text ? ' (' + text + ')' : '');
+        })
+        .trigger('summaryUpdated');
+
+      // Turn the legend into a clickable link, but retain span.fieldset-legend
+      // for CSS positioning.
+      var $legend = $('> legend .fieldset-legend', this);
+
+      $('<span class="fieldset-legend-prefix element-invisible"></span>')
+        .append($fieldset.hasClass('collapsed') ? Drupal.t('Show') : Drupal.t('Hide'))
+        .prependTo($legend)
+        .after(' ');
+
+      // .wrapInner() does not retain bound events.
+      var $link = $('<a class="fieldset-title" href="#"></a>')
+        .prepend($legend.contents())
+        .appendTo($legend)
+        .click(function () {
+          var fieldset = $fieldset.get(0);
+          // Don't animate multiple times.
+          if (!fieldset.animating) {
+            fieldset.animating = true;
+            Drupal.toggleFieldset(fieldset);
+          }
+          return false;
+        });
+
+      $legend.append(summary);
+    });
+  }
+};
+
+})(jQuery);
+;
+(function ($) {
+  
+  Drupal.behaviors.addtoany = {
+    attach: function(context, settings) {
+    
+      
+      // Initial page setup
+      
+      var addtoany_icon = $('input[value="a2a_svg_32"]').next('label').find('img:first'),
+        initial_icon_size_int = parseInt( $('input[name="addtoany_buttons_size"]:checked').val() );
+      
+      // Set the A2A icon's size to match selected Icon Size
+      addtoany_icon.height(initial_icon_size_int).width(initial_icon_size_int);
+      
+      
+      // Bring attention to large A2A icon option
+      // because the universal button will likely be changed to match the other icons
+      // (Drupal #states can't handle this)
+      $('input[name="addtoany_buttons_size"]').change(function() {
+      
+      	var icon_size = $(this).val(),
+      	  icon_size_int = parseInt(icon_size);
+        
+        // Expand Universal Button fieldset if collapsed
+        if ( $('#edit-universal-button').is('.collapsed') )
+          Drupal.toggleFieldset('#edit-universal-button');
+        
+        // Flash the A2A icon if not selected, because it should be selected to match the other icons
+        if ( icon_size == '32' && ! $('input[value="a2a_svg_32"]').is(':checked') )
+          addtoany_icon.fadeTo('fast', .2).fadeTo('fast', 1).fadeTo('fast', .2).fadeTo('fast', 1);
+        
+        // Set the A2A icon's size to match selected Icon Size
+        addtoany_icon.height(icon_size_int).width(icon_size_int);
+        
+      });
+      
+      
+    }
+  };
+
+}(jQuery));;
+(function ($) {
+
+Drupal.toolbar = Drupal.toolbar || {};
+
+/**
+ * Attach toggling behavior and notify the overlay of the toolbar.
+ */
+Drupal.behaviors.toolbar = {
+  attach: function(context) {
+
+    // Set the initial state of the toolbar.
+    $('#toolbar', context).once('toolbar', Drupal.toolbar.init);
+
+    // Toggling toolbar drawer.
+    $('#toolbar a.toggle', context).once('toolbar-toggle').click(function(e) {
+      Drupal.toolbar.toggle();
+      // Allow resize event handlers to recalculate sizes/positions.
+      $(window).triggerHandler('resize');
+      return false;
+    });
+  }
+};
+
+/**
+ * Retrieve last saved cookie settings and set up the initial toolbar state.
+ */
+Drupal.toolbar.init = function() {
+  // Retrieve the collapsed status from a stored cookie.
+  var collapsed = $.cookie('Drupal.toolbar.collapsed');
+
+  // Expand or collapse the toolbar based on the cookie value.
+  if (collapsed == 1) {
+    Drupal.toolbar.collapse();
+  }
+  else {
+    Drupal.toolbar.expand();
+  }
+};
+
+/**
+ * Collapse the toolbar.
+ */
+Drupal.toolbar.collapse = function() {
+  var toggle_text = Drupal.t('Show shortcuts');
+  $('#toolbar div.toolbar-drawer').addClass('collapsed');
+  $('#toolbar a.toggle')
+    .removeClass('toggle-active')
+    .attr('title',  toggle_text)
+    .html(toggle_text);
+  $('body').removeClass('toolbar-drawer').css('paddingTop', Drupal.toolbar.height());
+  $.cookie(
+    'Drupal.toolbar.collapsed',
+    1,
+    {
+      path: Drupal.settings.basePath,
+      // The cookie should "never" expire.
+      expires: 36500
+    }
+  );
+};
+
+/**
+ * Expand the toolbar.
+ */
+Drupal.toolbar.expand = function() {
+  var toggle_text = Drupal.t('Hide shortcuts');
+  $('#toolbar div.toolbar-drawer').removeClass('collapsed');
+  $('#toolbar a.toggle')
+    .addClass('toggle-active')
+    .attr('title',  toggle_text)
+    .html(toggle_text);
+  $('body').addClass('toolbar-drawer').css('paddingTop', Drupal.toolbar.height());
+  $.cookie(
+    'Drupal.toolbar.collapsed',
+    0,
+    {
+      path: Drupal.settings.basePath,
+      // The cookie should "never" expire.
+      expires: 36500
+    }
+  );
+};
+
+/**
+ * Toggle the toolbar.
+ */
+Drupal.toolbar.toggle = function() {
+  if ($('#toolbar div.toolbar-drawer').hasClass('collapsed')) {
+    Drupal.toolbar.expand();
+  }
+  else {
+    Drupal.toolbar.collapse();
+  }
+};
+
+Drupal.toolbar.height = function() {
+  var $toolbar = $('#toolbar');
+  var height = $toolbar.outerHeight();
+  // In modern browsers (including IE9), when box-shadow is defined, use the
+  // normal height.
+  var cssBoxShadowValue = $toolbar.css('box-shadow');
+  var boxShadow = (typeof cssBoxShadowValue !== 'undefined' && cssBoxShadowValue !== 'none');
+  // In IE8 and below, we use the shadow filter to apply box-shadow styles to
+  // the toolbar. It adds some extra height that we need to remove.
+  if (!boxShadow && /DXImageTransform\.Microsoft\.Shadow/.test($toolbar.css('filter'))) {
+    height -= $toolbar[0].filters.item("DXImageTransform.Microsoft.Shadow").strength;
+  }
+  return height;
+};
+
+})(jQuery);
+;
